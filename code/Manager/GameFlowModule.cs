965376c6a8064487079b5b8bcfe8627283d0d838
Guilhem PECH <guilhem.pech@gmail.com>
2023-07-22 18:29:43 +0200

    Add Hierarchical Finite State Machine (HFSM) for game flow
    This commit introduces a Hierarchical Finite State Machine to the game flow management module. The HFSM governs different states in the game, enhancing the control of game flow. States include waiting for players, in-game actions, such as preparing objectives and spawning players, and post-game states like voting for a map. This implementation will help to streamline game states and their transitions, ensuring a smooth gameplay experience. In addition, a new Utils file (HFSM.cs) is created to handle the hierarchical structure of the game states and their transitions.

--- code/Manager/GameFlowModule.cs
+++ code/Manager/GameFlowModule.cs
@@ -1,15 +1,92 @@
-﻿namespace Sandbox.Manager;
+﻿using Sandbox.Utils;
+
+namespace Sandbox.Manager;
 
 [Prefab]
 public class GameFlowModule : AGameModule
 {
+	private HFSM<States, TransitionEvents> m_hfsm;
+	
 	protected override void OnActivate()
 	{
+		BuildHFSM();
 		base.OnActivate();
 	}
 
 	protected override void OnDeactivate()
 	{
 		base.OnDeactivate();
-	} 
+	}
+
+
+	private void BuildHFSM()
+	{
+		HfsmBuilder<States, TransitionEvents> builder = new HfsmBuilder<States, TransitionEvents>();
+		
+		builder.AddState( States.WaitForPlayers );
+		{
+			builder.AddTransition( States.WaitForPlayers, States.InGame, TransitionEvents.Trigger );
+		}
+		builder.AddState( States.InGame );
+		{
+			builder.AddState( States.PrepareObjectives, States.InGame );
+			{
+				builder.AddTransition( States.PrepareObjectives, States.SpawnPlayers, TransitionEvents.Trigger );
+			}
+			builder.AddState( States.SpawnPlayers, States.InGame );
+			{
+				builder.AddTransition( States.SpawnPlayers, States.Gameplay, TransitionEvents.Trigger );
+			}
+			builder.AddState( States.Gameplay, States.InGame );
+			{
+				builder.AddState( States.Jerrycans, States.Gameplay );
+				{
+					builder.AddTransition( States.Jerrycans, States.Generator, TransitionEvents.Trigger );
+				}
+				builder.AddState( States.Generator, States.Gameplay );
+				{
+					builder.AddTransition( States.Generator, States.Radio, TransitionEvents.Trigger );
+				}
+				builder.AddState( States.Radio, States.Gameplay );
+				{
+					builder.AddTransition( States.Radio, States.Police, TransitionEvents.Trigger );
+				}
+				builder.AddState( States.Police, States.Gameplay );
+				
+				builder.AddTransition( States.Gameplay, States.Results, TransitionEvents.Trigger );
+			}
+			builder.AddState( States.Results );
+			{
+				builder.AddTransition( States.Results, States.VoteMap, TransitionEvents.Trigger );
+			}
+			builder.AddState( States.VoteMap );
+			{
+				builder.AddTransition( States.VoteMap, States.WaitForPlayers, TransitionEvents.Trigger );
+			}
+		}
+		m_hfsm = builder.Build();
+	}
+	
+	
+}
+
+enum States
+{
+	WaitForPlayers,
+	InGame,
+	PrepareObjectives,
+	SpawnPlayers,
+	Gameplay,
+	Jerrycans,
+	Generator,
+	Radio,
+	Police,
+	Results,
+	VoteMap
+};
+
+enum TransitionEvents
+{
+	Invalid,
+	Trigger
 }
