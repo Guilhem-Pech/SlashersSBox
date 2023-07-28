49ec2170de10d5f37f077ee7fda1cc90e3e1a7f4
Guilhem PECH <guilhem.pech@gmail.com>
2023-07-28 19:04:03 +0200

    Add Lerp functionality to BBox class
    Added a Lerp method to the BBoxExtensions class in Utils folder. The function allows linear interpolation between two bounding box objects' min and max points, which can be useful in transitioning between different sizes or positions of objects in the game minimizing the abrupt changes.

---
+++ code/Utils/ClassExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Sandbox.Utils;
+
+public static class BBoxExtensions 
+{
+	public static BBox Lerp(this BBox from, BBox to, float t, bool clamp = true)
+	{
+		Vector3 lerpMins = Vector3.Lerp(from.Mins, to.Mins, t, clamp);
+		Vector3 lerpMaxs = Vector3.Lerp(from.Maxs, to.Maxs, t, clamp);
+
+		return new BBox(lerpMins, lerpMaxs);
+	}
+}
