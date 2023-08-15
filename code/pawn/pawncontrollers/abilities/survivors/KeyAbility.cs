2d6e5bee7fe2a94bdf30d20b34cbb254eb46d68a
Guilhem PECH <guilhem.pech@gmail.com>
2023-08-15 12:31:22 +0200

    Add KeyAbility class for survivor pawn
    This commit introduces the 'KeyAbility' class, used to control the survivor pawn abilities in the game. It’s designed to extend the functionality by providing methods for activation, deactivation and simulation, laying the groundwork for implementing the ability to lock/unlock entities, likely doors, in the game play.

---
+++ code/pawn/pawncontrollers/abilities/survivors/KeyAbility.cs
@@ -0,0 +1,17 @@
+﻿namespace Sandbox.pawn.pawncontrollers.abilities.survivors;
+
+public class KeyAbility : IAbility, ISimulable
+{
+	public void OnActivate( Pawn pawn )
+	{
+	}
+
+	public void OnDeactivate()
+	{
+	}
+
+	public void Simulate( IClient client )
+	{
+		// TODO need to implement the logic of locking / unlocking a door entity
+	}
+}
